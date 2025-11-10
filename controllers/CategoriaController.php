36a58772258f46323483889142eb18b49da05899
Marcos Barcelar <marcosbarcelar1@gmail.com>
2025-11-09 23:49:13 -0300

    trabalho finalizado

--- controllers/CategoriaController.php
+++ controllers/CategoriaController.php
@@ -1,4 +1,17 @@
 <?php
+
+/************************************************
+25061077-2 - Kauã Ricardo Gomes Fagundes 
+25010975-2 - Bruno Petroli
+25004001-3 - Rafael Diesel
+25229846-2 - Wendel Souza Cardoso
+25228676-2 - Marlon Willian Silva Barros
+25357682-2 - Eduardo Rupp da Luz
+25165088-2 - Vinicius Bastos Rodrigues
+25178065-2 - Marcos Barcelar
+22001126-2 - Eric Ruthers
+***********************************************/
+
 include_once '../config/database.php';
 include_once '../models/Categoria.php';
 
---
+++ views/includes/footer.php
@@ -0,0 +1,27 @@
+
+<!-- /************************************************
+25061077-2 - Kauã Ricardo Gomes Fagundes 
+25010975-2 - Bruno Petroli
+25004001-3 - Rafael Diesel
+25229846-2 - Wendel Souza Cardoso
+25228676-2 - Marlon Willian Silva Barros
+25357682-2 - Eduardo Rupp da Luz
+25165088-2 - Vinicius Bastos Rodrigues
+25178065-2 - Marcos Barcelar
+22001126-2 - Eric Ruthers
+***********************************************/ -->
+
+</main>
+
+<footer>
+    <div class="footer-content">
+        <p>&copy; <?php echo date("Y"); ?> Loja. Todos os direitos reservados.</p>
+        <p>Contato: (11) 1234-5678 | Email: contato@loja.com</p>
+        <div class="social-media">
+            <a href="#">Facebook</a> | <a href="#">Instagram</a> | <a href="#">Twitter</a>
+        </div>
+    </div>
+</footer>
+</body>
+
+</html>
